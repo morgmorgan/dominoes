d8e4b15a59000e0f95e3378f8ccf2e4c24388c72
Jane Carter <janeware3242@gmail.com>
2025-06-17 15:15:54 -0700

    enable physics on tip over end

--- scripts/domino.gd
+++ scripts/domino.gd
@@ -3,9 +3,17 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimationPlayer.play("tip_over")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
+
+
+func _on_domino_end(anim_name: StringName) -> void:
+	if name == "tip_over":
+		$physisc/RigidBody3D.freeze = false
+	
