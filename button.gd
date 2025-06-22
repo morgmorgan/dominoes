868c4dbe1d2f19a0246ed80b3776df31ac643842
Jane Carter <janeware3242@gmail.com>
2025-06-22 11:34:22 -0700

    i'm stupid and this file didn't save

--- button.gd
+++ button.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+signal button_changed(pressed: bool)
+
 const GREEN_MATERIAL : StandardMaterial3D = preload("res://resources/gridtile_clicked.tres")
 const RED_MATERIAL : StandardMaterial3D = preload("res://resources/gridtile_rightClicked.tres")
 
@@ -17,8 +19,18 @@ func hold_button_down() -> void:
 	$AnimationPlayer.play("button_pressed")
 	$Cylinder_001.set_surface_override_material(0, GREEN_MATERIAL)
 	button_pressed = true
+	button_changed.emit(button_pressed)
 	
 func unpress_button() -> void:
 	$AnimationPlayer.play("button_pressed", -1, -1.0, true)
 	$Cylinder_001.set_surface_override_material(0, RED_MATERIAL)
-	button_pressed = true
+	button_pressed = false
+	button_changed.emit(button_pressed)
+
+func _on_area_3d_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+	hold_button_down()
+	pass # Replace with function body.
+
+func _on_area_3d_area_shape_exited(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+	unpress_button()
+	pass # Replace with function body.
